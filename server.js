986d49a8531139cce8b2128a3d3e12cdb4e9e9d1
dhope21 <dhope21@gmail.com>
2017-12-26 20:01:07 +0530

    added winston logger in a seperate file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db = require('./db/index');
+const logger = require('./logger');
 
 db.connect();
 
@@ -11,12 +12,14 @@ app.get('/', (req, res) => res.send("Welcome to Node Postgres Express POC !!"))
 
 app.get('/causes', (req, res, next) => db.getCauses((causes, err) => {
 	if(causes){
+		logger.loginto('inside if cause');
 		return next(causes);
 	};
+	logger.loginto('exiting get cause');
 }));
 
 
-
+logger.loginto('trying this logger');
 
 
 const port = process.env.PORT || 3000;
