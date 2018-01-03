29bed38d5ce90b29475da99dfc2296a6b42ecf37
nishant khandelwal <nishantkhandelwal69@gmail.com>
2018-01-03 13:46:18 +0530

    merge with master except server.js

---
+++ newrelic.js
@@ -0,0 +1,25 @@
+'use strict'
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['node-postgres'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '395c8225f0f69e00679fa68f0657b532b1b97a42',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
