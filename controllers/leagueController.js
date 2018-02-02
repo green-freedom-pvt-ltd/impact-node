99f19f98add79bd2de355f9e1f1232e6b06d54ce
nishant khandelwal <nishantkhandelwal69@gmail.com>
2018-02-02 13:54:41 +0530

    reformat league api

--- controllers/leagueController.js
+++ controllers/leagueController.js
@@ -8,21 +8,27 @@ const env = require('../config/settings');
 const paginconfig = env.pagination;
 
 const filterList = [];
+const parameterTypes = {};
+
+
+var fields = ["id", "impactleague_name","impactleague_banner", "duration", "start_date","end_date", "is_active","team_size","impactleague_banner_site","impactleague_description_site","email_type","module", ["company_id","company"]]
 
 var League = {
 
     //get single league
     getLeague(req, res) {
-        var league = req.params.id;
-        league = parseInt(league);
-
-
-
+      
         var urlQuery = req.query;
-        var whereQuery = pagin.createQuery(urlQuery, filterList);
-        logger.info("Where query", whereQuery);
+        try {
+            var whereQuery = pagin.createQuery(urlQuery, filterList, parameterTypes);
+        } catch (err) {
+            // res.send({Error:err},400);
+            res.status(400).send({error:err})
+            throw err;
+        }
         return db.impactLeague.findAndCountAll({
             where: whereQuery,
+            attributes:fields, 
             limit: paginconfig.SMALL,
             offset: (urlQuery.page == 0 || (isNaN(urlQuery.page)) ? 1 : urlQuery.page == 1) ? 0 : ((urlQuery.page - 1) * paginconfig.SMALL)
 
