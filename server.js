a151b09a85cf8a0655b29cae083614abaaebd0f7
dhope21 <dhope21@gmail.com>
2018-02-21 13:48:20 +0530

    added clustering

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('newrelic');
+var cluster = require('cluster');
 const express = require('express');
 // const db = require('./db/index');
 const appAuth = require('./middleware/authenticate/app');
@@ -28,8 +29,25 @@ app.use('/', routes);
 app.get('/causespromise', (req, res, next) => res.send(db.getImportantData()));
 
 
-const port = process.env.PORT || 8000;
-app.listen(port, () => console.log('listening on port ' + port));
+
+// clustering for server is done here
+// we count the number of cpu cores and
+// run node process on each cpu
+var numCPUs = require('os').cpus().length;
+if (cluster.isMaster) {
+    // Fork workers.
+    // console.log('numCPUs--------',numCPUs);
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+    cluster.on('exit', function (worker, code, signal) {
+        console.log('worker ' + worker.process.pid + ' died');
+    });
+} else {
+    console.log(cluster.worker.id);
+    const port = process.env.PORT || 8000;
+    app.listen(port, () => console.log('listening on port ' + port));
+}
 
 
 module.exports = app
