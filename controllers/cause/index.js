774ff8c09e52542a42a0a1c70e7ee685adc7ec67
nishant khandelwal <nishantkhandelwal69@gmail.com>
2018-04-21 19:31:13 +0530

    Added currency in cause API

--- controllers/cause/index.js
+++ controllers/cause/index.js
@@ -148,7 +148,7 @@ function getSplitedData(data, mapping) {
 
     })
 
-   // console.log("splitted_data", splitted_data);
+    // console.log("splitted_data", splitted_data);
     return splitted_data;
 }
 
@@ -201,6 +201,97 @@ var causeModel = {
     async getCauses(req, res) {
         let final_data = {};
         let cause_result = [];
+
+
+        let exchange_rate = [
+            {
+                "currency": "USD",
+                "rate": 65
+            },
+            {
+                "currency": "CAD",
+                "rate": 52
+            },
+            {
+                "currency": "INR",
+                "rate": 1
+            },
+            {
+                "currency": "GBP",
+                "rate": 86.11
+            },
+            {
+                "currency": "EUR",
+                "rate": 77.21
+            },
+            {
+                "currency": "JPY",
+                "rate": 0.58
+            },
+            {
+                "currency": "AUD",
+                "rate": 50.94
+            },
+            {
+                "currency": "CHF",
+                "rate": 66.83
+            },
+            {
+                "currency": "CNY",
+                "rate": 9.88
+            },
+            {
+                "currency": "SEK",
+                "rate": 8.04
+            },
+            {
+                "currency": "NZD",
+                "rate": 74.21
+            },
+            {
+                "currency": "MXN",
+                "rate": 3.48
+            },
+            {
+                "currency": "SGD",
+                "rate": 48.08
+            },
+            {
+                "currency": "HKD",
+                "rate": 8.34
+            },
+            {
+                "currency": "NOK",
+                "rate": 8.23
+            },
+            {
+                "currency": "KRW",
+                "rate": 0.06
+            },
+            {
+                "currency": "TRY",
+                "rate": 17.9
+            },
+            {
+                "currency": "RUB",
+                "rate": 1.13
+            },
+            {
+                "currency": "BRL",
+                "rate": 20.52
+            },
+            {
+                "currency": "ZAR",
+                "rate": 4.83
+            }
+        ]
+
+        let get_overall_cause = await sequelize.query('select 1 as run_id, sum(run_amount) as overall,count(run_id) as run_count, sum(no_of_steps) as step_count from share_api_runs where is_flag=false', {
+            type: sequelize.QueryTypes.SELECT
+        })
+
+        let result = await get_overall_cause;
+
         let get_causes = await sequelize.query(GET_ALL_CAUSE,
             {
                 // replacements: { limit: env.pagination.MEDIUM },
@@ -210,28 +301,46 @@ var causeModel = {
         )
         let cause_response = JSON.parse(JSON.stringify(get_causes))
 
+        
+        //console.log(result);
+        // .then(results=>{
+        //     return results;
+        // })
+        // .catch(err=>{
+        //     console.log("came in catch",err);
+        //     return err;
+        // })
 
-
-        if (cause_response.length > 0) {
-
-
+        if (cause_response.length > 0 && get_overall_cause.length > 0) {
+            let get_data = result[0];
             let new_value = cause_response.map((current, index) => {
-                console.log("current", current);
+                //  console.log("current", current);
                 let cause = {};
                 cause = current;
                 //console.log("current", current.cause_thank_you_image_v2);
                 let get_cause_images = getSplitedData(current.cause_thank_you_image_v2, cause_thank_you_image_v2);
-                console.log("get_cause_images", get_cause_images);
+                // console.log("get_cause_images", get_cause_images);
                 cause["cause_thank_you_image_v2"] = get_cause_images;
                 cause_result.push(cause)
                 //console.log("cause", cause);
 
             })
-            // console.log("cause_result", cause_result);
+            //  console.log("cause_result", cause_result);
 
             final_data.count = cause_response.length;
+
+
             final_data.results = cause_result;
+            final_data["exchange_rates"] = exchange_rate;
+
+            final_data["overall_impact"] = get_data.overall
+            final_data["overall_num_steps"] = get_data.step_count;
+            final_data["overall_num_runs"] = get_data.run_count;
             res.json(final_data);
+        }
+        else {
+           logger.info("Data not came thought cause API",cause_response,result);
+           res.status(403).send("Unable to get data. Please contact admin");
         }
 
         // res.json(cause_response);
