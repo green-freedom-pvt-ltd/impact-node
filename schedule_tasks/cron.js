376504e7aadb44ec7daf5c25822c4bb9724f66dc
nishant khandelwal <nishantkhandelwal69@gmail.com>
2018-04-29 16:33:23 +0530

    node cron test

--- schedule_tasks/cron.js
+++ schedule_tasks/cron.js
@@ -12,7 +12,7 @@ let _ = require('underscore');
 
 let INSERTION_TEAMLEDERBOARD = `
 insert into public.share_api_teamleaderboard(team_id,user_id,total_amount,total_distance,run_count)
-select team_id_id,user_id_id,sum(run_amount),sum(distance),count(run_id) from share_api_runs
+select team_id_id,user_id_id,sum(run_amount),sum(distance),count(run_id) from share_api_runn
 where team_id_id IS Not NULL AND is_flag= false
 group by team_id_id,user_id_id
 order by sum(run_amount) desc;`
@@ -29,7 +29,7 @@ order by sum(run_amount) desc;`
 // cron.nextInvocation(new Date('2018','04','28','13','17'));
 // cron.on()
 // console.log(cron.nextInvocation(new Date('2018','04','28','13','15')));
-let cron = new CronJob('* * * * *', function () {
+let cron = new CronJob('*/10 * * * * *', function () {
 
 
     // return sequelize.query(INSERTION_TEAMLEDERBOARD, {
@@ -43,32 +43,55 @@ let cron = new CronJob('* * * * *', function () {
     //     console.log("ERROR",err);
     // });
 
-    return sequelize.transaction(function (t) {
 
-        // chain all your queries here. make sure you return them.
-        return db.teamleaderboard.destroy(
-            { truncate: true }
-        ).then(function (result) {
-            console.log("detroyed successfully", result);
-            return sequelize.query(INSERTION_TEAMLEDERBOAR, {
-                type: sequelize.QueryTypes.SELECT
+    sequelize.transaction(transaction => { // Note that we use a callback rather than a promise.then()
+        return db.teamleaderboard.destroy({ truncate: true })
+            .then(user => {
+                return sequelize.query(INSERTION_TEAMLEDERBOARD, {
+                    type: sequelize.QueryTypes.INSERT
+                }, { transaction })
             })
-            .then(final=>{
-                console.log("updated")
-            })
-            // Transaction has been committed
-            // result is whatever the result of the promise chain returned to the transaction callback
-        }).catch(function (err) {
-            console.log("error occured", err);
-            t.rollback();
-            // Transaction has been rolled back
-            // err is whatever rejected the promise chain returned to the transaction callback
-        });
+          
+    }).then((result) => {
+        console.log("committed")
+        // Committed
+    }).catch(err => {
+        // Rolled back
+        console.log("Rollback");
+        console.error(err);
+    });
+
+    // return sequelize.transaction(function (t) {
+
+    //     // chain all your queries here. make sure you return them.
+    //     return db.teamleaderboard.destroy(
+    //         { truncate: true }
+    //     ).then(function (result) {
+    //         console.log("detroyed successfully", result);
+    //         sequelize.query(INSERTION_TEAMLEDERBOARD, {
+    //             type: sequelize.QueryTypes.INSERT
+    //         })
+
+    //             .then(final => {
+    //                 console.log("updated")
+    //             })
+    //             .catch(err => {
+    //                 console.log("errir", err);
+    //             })
+
+    //         // Transaction has been committed
+    //         // result is whatever the result of the promise chain returned to the transaction callback
+    //     }).catch(function (err) {
+    //         console.log("error occured", err);
+    //         t.rollback();
+    //         // Transaction has been rolled back
+    //         // err is whatever rejected the promise chain returned to the transaction callback
+    //     });
 
 
 
     }, null, false, 'Asia/Kolkata');
-})
+
 
 let cron1 = new CronJob(' * * * * *', function () {
     console.log('You will see this message every second');
